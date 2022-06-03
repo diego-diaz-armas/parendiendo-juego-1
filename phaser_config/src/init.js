eacf2f1e9c2a7c5b0b02242feff3daa662f41166
diego <diego.diaz.armas@gmail.com>
2022-06-02 22:40:56 -0300

    "Entrar"

--- phaser_config/src/init.js
+++ phaser_config/src/init.js
@@ -7,6 +7,14 @@ const config={
             preload: preload,
             create: create,
             update: update
+        },
+        physics:{
+            default: "arcade",
+            arcade: {
+                gravity:{
+                    y:500
+                }
+            }
         }
 }
 var game =new Phaser.Game(config);
@@ -16,11 +24,30 @@ console.log("soy preload 1");
 //en preolad se renderizan las imagenes
     this.load.image("bird","/assets/bird.png");
     this.load.image("bird2","/assets/bird_dos.png");
+    this.load.image("bird3","/assets/bird.png");
+    this.load.image("bird4","/assets/bird_dos.png");
 }
 function create(){
 console.log("soy create");
-this .bird = this.add.image(50,50,"bird");
-this .bird2 = this.add.image(550,50,"bird2");
+this.bird=this.physics.add.image(275,50,"bird");//este es el pajaro quese cae 
+this.bird.setCollideWorldBounds (true);
+
+this.bird2=this.physics.add.image(350,50,"bird2");//este es el pajaro quese cae 
+this.bird2.setCollideWorldBounds (true);
+this.bird2.flipX=true;
+
+
+this.bird4 = this.add.image(50,50,"bird3");
+this.bird4.setScale(1);
+   this.bird4.flipX=false;
+   
+  
+
+this.bird3 = this.add.image(550,50,"bird4");
+this.bird3.setScale(1);
+   this.bird3.flipX=true;
+  
+
    // console.log(this.bird);//para ver las propiedades del pajaro en proto veremos lo que puede hacer  bird
    this.bird.setScale(1);
    this.bird.flipX=true;//lo rota
@@ -39,4 +66,8 @@ function update(time,delta){
     //console.log(delta);
     this.bird.angle++;
     this.bird2.angle--;
+    
+    this.bird4.x++;
+    this.bird3.x--;
+   
 }
